74b6295ad5b8fda555ce1825f98c76f626d98dd5
User <justin.the@gmail.com>
2021-10-11 14:42:33 +0700

    uncomment SQLALCHEMY_TRACK_MODIFICATIONS

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ db = SQLAlchemy()
 
 def setup_db(app, database_path=db_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-#    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_ECHO"] = True
     db.app = app
     db.init_app(app)
