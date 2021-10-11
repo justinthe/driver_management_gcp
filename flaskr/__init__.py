a10351282d35a39a0df4e3f0a2a12c902997c387
User <justin.the@gmail.com>
2021-10-11 10:43:43 +0700

    updated to flaskr folder

---
+++ flaskr/__init__.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, jsonify
+# from flask_cors import CORS
+import psycopg2
+
+db_user = os.environ.get('CLOUD_SQL_USERNAME')
+db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+app = Flask(__name__)
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    @app.route('/')
+    def init():
+        if os.environ.get('GAE_ENV') == 'standard':
+            host = '/cloudsql/{}'.format(db_connection_name)
+        else:
+            host = '127.0.0.1'
+
+        cnx = psycopg2.connect(dbname=db_name, user=db_user,
+                            password=db_password, host=host)
+
+        with cnx.cursor() as cursor:
+            cursor.execute('SELECT NOW() as now;')
+            result = cursor.fetchall()
+
+        current_time = result[0][0]
+        cnx.commit()
+        cnx.close()
+
+        return str(current_time)
+
+    return app
+
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1', port=8080, debug=True)
