4086d8c964d3192ab98db6dd028ef65cd55e9e31
User <justin.the@gmail.com>
2021-10-11 14:15:17 +0700

    added sqlalchemy

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 import psycopg2
+from models import *
+import sys
 
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
 db_password = os.environ.get('CLOUD_SQL_PASSWORD')
@@ -23,27 +25,41 @@ def create_app(test_config=None):
 
     @app.route('/')
     def init():
-        if os.environ.get('GAE_ENV') == 'standard':
-            host = '/cloudsql/{}'.format(db_connection_name)
-        else:
-            host = '127.0.0.1'
+        # if os.environ.get('GAE_ENV') == 'standard':
+        #     host = '/cloudsql/{}'.format(db_connection_name)
+        # else:
+        #     host = '127.0.0.1'
 
-        cnx = psycopg2.connect(dbname=db_name, user=db_user,
-                            password=db_password, host=host)
+        # cnx = psycopg2.connect(dbname=db_name, user=db_user,
+        #                     password=db_password, host=host)
 
-        with cnx.cursor() as cursor:
-            cursor.execute('SELECT NOW() as now;')
-            result = cursor.fetchall()
+        # with cnx.cursor() as cursor:
+        #     cursor.execute('SELECT NOW() as now;')
+        #     result = cursor.fetchall()
 
-        current_time = result[0][0]
-        cnx.commit()
-        cnx.close()
+        # current_time = result[0][0]
+        # cnx.commit()
+        # cnx.close()
+
+        sql = "SELECT NOW() as now;"
+        try:
+            data = DBUtil.call_raw_sql(sql)
+            return jsonify({
+                    'success': True,
+                    'time': str(data)
+                })
+        except:
+            msg = sys.exc_info()
+            return jsonify({
+                    'success': False, 
+                    'message': str(msg)
+                })
 
         # return str(current_time)
-        return jsonify({
-        		'success': True,
-        		'time': str(current_time)
-        	})
+        # return jsonify({
+        # 		'success': True,
+        # 		'time': str(current_time)
+        # 	})
 
     return app
 
---
+++ models.py
@@ -0,0 +1,32 @@
+import os
+from datetime import datetime 
+from sqlalchemy import Column, String, Integer, create_engine, Boolean, \
+        ForeignKey, DateTime, func, text
+from sqlalchemy.orm import relationship
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+
+db_user = os.environ.get('CLOUD_SQL_USERNAME')
+db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+db_path = "postgresql+pg8000://{}:{}@/{}?unix_sock=cloudsql/{}/.s.PGSQL.5432".format(db_user, db_password, db_name, db_connection_name)
+
+db = SQLAlchemy()
+
+def setup_db(app, database_path=db_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ECHO"] = True
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class DBUtil():
+    def call_raw_sql(sql):
+        statement = text(sql)
+        data = db.session.execute(statement)
+        db.session.commit()
+        return data
